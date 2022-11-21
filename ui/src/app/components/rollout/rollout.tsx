66bc300aed14e78676a3e8655fbabe54c1edb04f
vvtelu <satya@opsmx.io>
2022-11-21 10:46:24 +0530

    feat: scrollto fix

--- ui/src/app/components/rollout/rollout.tsx
+++ ui/src/app/components/rollout/rollout.tsx
@@ -100,7 +100,10 @@ export const RolloutWidget = (props: {rollout: RolloutRolloutInfo; interactive?:
     const passDataToRollout = (data:any) => {
         setreport(data.showReports);
         setData(data);
-        window.scrollTo(0,0);
+        const divElement = document.getElementById('reportId');
+        if(divElement){
+            divElement.scrollTop = 0;
+        }
       }
     for (const img of images) {
         for (const container of rollout.containers || []) {
@@ -113,7 +116,7 @@ export const RolloutWidget = (props: {rollout: RolloutRolloutInfo; interactive?:
     return (
         <React.Fragment>
                 {showreport && 
-               <ThemeDiv className='report-bg reports__info'>
+               <ThemeDiv id ='reportId' className='report-bg reports__info'>
                    <ReportsWidget clickback={()=> setreport(false)} reportsInput={revisionData}/>
                 </ThemeDiv>}
             <div className='rollout__row rollout__row--top'>
