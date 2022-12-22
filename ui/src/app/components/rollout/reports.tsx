e9a0494d40d725b8ee655d4f03aaeb4a4a9ffd02
vvtelu <satya@opsmx.io>
2022-12-22 18:32:53 +0530

    handling multiple jobs

--- ui/src/app/components/rollout/reports.tsx
+++ ui/src/app/components/rollout/reports.tsx
@@ -17,6 +17,13 @@ export const ReportsWidget = (props: {  clickback: any; reportsInput: {}}) => {
         return false;
       }
     }
+    // const getJobs = (props:any) => {
+    //   const dd = [];
+    //         //  const mResults = b.status?.metricResults;
+    //             props.forEach((element:any) => {
+    //               f.push()
+    //           });
+    // }
     const LoadApiCalls = (props: any) => {
       // console.log('1stapi',props);
         setLoading(true);
@@ -24,15 +31,25 @@ export const ReportsWidget = (props: {  clickback: any; reportsInput: {}}) => {
         let url2 = '/api/v1/applications/' + props.reportsInput.appName + '/resource?name=' + props.reportsInput.resourceName + '&appNamespace=' + props.reportsInput.nameSpace + '&namespace=' + props.reportsInput.nameSpace + '&resourceName=' + props.reportsInput.resourceName + '&version=' + props.reportsInput.version + '&kind=AnalysisRun&group=argoproj.io';
         fetch(url2)
           .then(response => {
-            return response.json()
+            return response.json();
           })
           .then((data: any) => {
             if (data.manifest.includes('job-name')) {
               let b = JSON.parse(data.manifest);
               // console.log(b);
-              if (b.status?.metricResults[b.status.metricResults.length - 1]?.measurements[b.status.metricResults.length - 1]?.metadata['job-name']) {
-                fetchEndpointURL(props.reportsInput.appName, props.reportsInput.resourceName, props.reportsInput.nameSpace, props.reportsInput.version, b.status?.metricResults[b.status.metricResults.length - 1]?.measurements[b.status.metricResults.length - 1]?.metadata['job-name']);
-              }
+              //getJobs(b.status?.metricResults);
+              const newJobs = [];
+              const mResults = b.status?.metricResults;
+                mResults.forEach((element:any,index:number) => {
+                 // if(element.name.includes('opsmx')){
+                    newJobs.push(element.measurements[0]?.metadata['job-name']);
+                    if (element.measurements[0]?.metadata['job-name']) {
+                      fetchEndpointURL(props.reportsInput.appName, props.reportsInput.resourceName, props.reportsInput.nameSpace, props.reportsInput.version, element.measurements[0]?.metadata['job-name'],index);
+                    }
+                 // }
+                });
+
+             
             }else{
               setValidUrl(false);
               setLoading(false);
@@ -43,19 +60,20 @@ export const ReportsWidget = (props: {  clickback: any; reportsInput: {}}) => {
           });
       };
 
-    const fetchEndpointURL = (applicationName: String, resouceName: String, nameSpace: String, version: String, jobName: String) => {
+    const fetchEndpointURL = (applicationName: String, resouceName: String, nameSpace: String, version: String, jobName: String, index:number) => {
         let url3 = '/api/v1/applications/' + applicationName + '/resource?name=' + jobName + '&appNamespace=' + nameSpace + '&namespace=' + nameSpace + '&resourceName=' + jobName + '&version=v1&kind=Job&group=batch'
         fetch(url3)
           .then(response => {
             return response.json()
           })
           .then((data: any) => {
+            console.log(index);
             if (data.manifest.includes('message')) {
               let a = JSON.parse(data.manifest);
               // console.log(a);
               var indexValue = a.status.hasOwnProperty('succeeded')? a.status.conditions.length - 2: a.status.conditions.length - 1;
               // console.log(a.status.conditions[indexValue].message);
-              if (a.status?.conditions[indexValue]?.message) {
+              if (a.status?.conditions[indexValue]?.message && a.status?.condition[indexValue]?.type == 'OpsmxAnalysis') {
                 let stringValue2 = a.status?.conditions[indexValue]?.message.split(/\n/)[4];
                 let stringValue = a.status?.conditions[indexValue]?.message.split(/\n/)[3];
                 // let stringValue1 = a.status?.conditions[indexValue]?.message.split(/\n/)[1];
@@ -111,33 +129,32 @@ export const ReportsWidget = (props: {  clickback: any; reportsInput: {}}) => {
       return (
       <WaitFor loading={loading}>
         <div style={{ margin: '1em', width: '100%', height: '100%' }}>
-          <div className='bc-element bc-first' style={{ left: '0px' }} onClick={() => props.clickback()}>
-            <div className='bc-text bc-text-first addPointer'>Back to Dashboard</div>
-            <div className='bc-arrow' style={{ zIndex: 2 }}></div>
-          </div>
-          <div className='bc-element' style={{ left: '-5px' }}>
-            <div className='bc-before-arrow bc-hefore-arrow-last'>
-              <div className='bc-arrow' style={{ borderLeft: '10px solid white' }}></div>
+            <div className='bc-element bc-first' style={{ left: '0px' }} onClick={() => props.clickback()}>
+              <div className='bc-text bc-text-first addPointer'>Back to Dashboard</div>
+              <div className='bc-arrow' style={{ zIndex: 2 }}></div>
             </div>
-            <div className='bc-text bc-text-last'>{analysisName}
-            </div>
-            <div className='bc-arrow bc-arrow-last' style={{ zIndex: 2 }}></div>
-          </div>
-          <div style={{ clear: 'both' }}></div>
-          {validUrl && <iframe src={getURL} width="100%" height="90%"></iframe>}
-          {!validUrl && 
-          <div style={{width: '100%', height: '100%', backgroundColor:'#dae1eb'}}>
-            <div className="reports__error">
-              <div className="empty-state">
-                <div className="empty-state__icon">
-                  <i className="fa fa-file"></i>
-                </div>
-                <h4 style={{color:'#155362'}}>Analysis not performed or Report not available</h4>
-                <h5></h5>
+            <div className='bc-element' style={{ left: '-5px' }}>
+              <div className='bc-before-arrow bc-hefore-arrow-last'>
+                <div className='bc-arrow' style={{ borderLeft: '10px solid white' }}></div>
               </div>
+              <div className='bc-text bc-text-last'>{analysisName}
+              </div>
+              <div className='bc-arrow bc-arrow-last' style={{ zIndex: 2 }}></div>
             </div>
-          </div>}
-        </div>
-    </WaitFor>
+            <div style={{ clear: 'both' }}></div>
+            {validUrl && <iframe src={getURL} width="100%" height="90%"></iframe>}
+            {!validUrl && 
+            <div style={{width: '100%', height: '100%', backgroundColor:'#dae1eb'}}>
+              <div className="reports__error">
+                <div className="empty-state">
+                  <div className="empty-state__icon">
+                    <i className="fa fa-file"></i>
+                  </div>
+                  <h5 style={{color:'#155362'}}>Analysis not performed or Report not available</h5>
+                </div>
+              </div>
+            </div>}
+          </div>
+      </WaitFor>
       );
 };
